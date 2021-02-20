de809ab809129c2ff7398495c986354a275dd2dc
Erdinç Ekşi <erdinceksii@gmail.com>
2021-02-20 22:08:34 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,74 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            var addToCar = _carDal.Get(c=>c.Id == car.Id);
+            if (addToCar == null)
+            {
+                if (car.Description.Length > 2)
+                {
+                    if (car.DailyPrice > 0)
+                    {
+                        _carDal.Add(car);
+                        Console.WriteLine("Car {0} added to the list.", car.Description);
+                    }
+                    else
+                    {
+                        Console.WriteLine("DailyPrice : {0} must be greater than 0", car.DailyPrice);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("CarDescription : {0} must be greater than 2", car.DailyPrice);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Car {0} already exists in the list.", car.Description);
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            if (_carDal.GetAll().Count == 0)
+            {
+                Console.WriteLine("\nNo car in the list.\n");
+            }
+            else
+            {
+                _carDal.Delete(car);
+            }
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            var carList = new List<Car> {
+                new Car{BrandId = 1, ColorId = 1, ModelYear = 2017, DailyPrice = 185000, Description = "Toyota Corolla", Id = 1},
+                new Car{BrandId = 2, ColorId = 1, ModelYear = 2016, DailyPrice = 750000, Description = "Mercedes C 180", Id = 2},
+                new Car{BrandId = 3, ColorId = 2, ModelYear = 2019, DailyPrice = 805000, Description = "BMW 3.20i", Id = 3},
+                new Car{BrandId = 4, ColorId = 4, ModelYear = 2016, DailyPrice = 530000, Description = "Jaguar XE 2.0D R-SPORT", Id = 4},
+                new Car{BrandId = 5, ColorId = 5, ModelYear = 2006, DailyPrice = 600000, Description = "Honda S2000", Id = 5}
+            };
+
+            foreach (var car in carList)
+            {
+                carManager.Add(car);
+            }
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Car Id: {0}, Brand Id: {1}, ColorId: {2}, DailyPrice: {3}, Description: {4}, ModelYear: {5}",
+                    car.Id, car.BrandId, car.ColorId, car.DailyPrice, car.Description, car.ModelYear);
+            }
+        }
+    }
+}
